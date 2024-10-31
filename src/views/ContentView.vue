b830969faba709e07b52b5d82d3992abf01eb9c2
Maxime HEIM <maxime.heim2023@campus-eni.fr>
2024-10-31 13:11:48 +0100

    remove unused content

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -16,7 +16,6 @@ const props = defineProps({
   }
 })
 </script>
-
 <template>
   <div class="p-6 bg-gray-100 min-h-screen">
     <div class="max-w-7xl mx-auto">
@@ -41,30 +40,4 @@ const props = defineProps({
     </div>
   </div>
 </template>
-
-<!--<template>-->
-<!--  <div v-if="loading" class="h-56 grid grid-cols-3 gap-4">-->
-<!--    <div class="flex justify-center" v-for="item in data" :key="item.id">-->
-<!--      <a-->
-<!--        :href="item.url"-->
-<!--        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"-->
-<!--      >-->
-<!--        <h5-->
-<!--          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"-->
-<!--        >-->
-<!--          {{ item.title }}-->
-<!--        </h5>-->
-<!--        <p class="font-normal text-gray-700 dark:text-gray-400">-->
-<!--          {{ item.description }}-->
-<!--        </p>-->
-<!--        <div class="grid grid-cols-4">-->
-<!--          <div  v-for="tag in item.tags" :key="tag.id">-->
-<!--            <img class="rounded-md " :src="`../logo/${tag}.png`">-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </a>-->
-<!--    </div>-->
-<!--  </div>-->
-<!--</template>-->
-
 <style scoped></style>

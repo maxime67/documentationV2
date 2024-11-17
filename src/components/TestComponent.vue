b3858e4719f71a343174d5b73689953695275094
maxime67 <maxime.heim3@gmail.com>
2024-11-17 12:39:49 +0100

    new category management

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,60 +1,5 @@
 <template>
-  <nav class="block w-full  px-4 py-2 mx-auto bg-white shadow-md lg:px-8 lg:py-3 ">
-    <div class="flex flex-wrap items-center justify-between mx-auto text-slate-800">
-      <a href="#" class="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
-        <img class="w-28 rounded-md" :src="`../logo/LogoEasySetup.png`" />
-      </a>
-
-      <div class="hidden lg:block">
-        <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-          <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-slate-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-            </svg>
-
-            <a href="#" class="flex items-center">
-              Pages
-            </a>
-          </li>
-          <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-slate-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-
-            <a href="#" class="flex items-center">
-              Account
-            </a>
-          </li>
-          <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-slate-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
-            </svg>
-
-            <a href="#" class="flex items-center">
-              Blocks
-            </a>
-          </li>
-          <li
-              class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-slate-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-            </svg>
-
-            <a href="#" class="flex items-center">
-              Docs
-            </a>
-          </li>
-        </ul>
-      </div>
-      <button class="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden" type="button">
-      <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-        </svg>
-      </span>
-      </button>
-    </div>
-  </nav>
+  <NavbarView></NavbarView>
   <div class="flex min-h-screen bg-gray-100">
     <!-- Backdrop overlay - for mobile only -->
     <div
@@ -62,7 +7,6 @@
         class="fixed inset-0 bg-gray-600 bg-opacity-50 transition-opacity sm:hidden"
         @click="closeSidebar"
     ></div>
-
     <!-- Sidebar -->
     <aside
         :class="[
@@ -72,16 +16,16 @@
         aria-label="Sidebar"
     >
       <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
-        <div v-for="(subcategories, category) in categories" :key="category" class="mb-2">
+        <div v-for="category in categories" :key="category.name" class="mb-2">
           <!-- Category dropdown button -->
           <button
-              @click="toggleCategory(category)"
+              @click="toggleCategory(category.name)"
               class="w-full flex items-center justify-between p-2 text-gray-900 rounded-lg hover:bg-gray-100"
           >
-            <span class="font-medium">{{ category }}</span>
+            <span class="font-medium">{{ category.name }}</span>
             <svg
                 class="w-4 h-4"
-                :class="{ 'transform rotate-180': openCategory === category }"
+                :class="{ 'transform rotate-180': openCategory === category.name }"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -97,11 +41,11 @@
 
           <!-- Subcategories dropdown -->
           <div
-              v-show="openCategory === category"
+              v-show="openCategory === category.name"
               class="ml-4 mt-1 space-y-1"
           >
             <a
-                v-for="subcategory in subcategories"
+                v-for="subcategory in category.subcategories"
                 :key="subcategory"
                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
@@ -112,30 +56,30 @@
               >
                 <input
                     type="checkbox"
-                    :key="subcategory"
-                    :checked="isSubcategorySelected(category, subcategory)"
-                    @change="toggleSubcategory(category, subcategory)"
+                    :id="subcategory"
+                    :checked="selectedSubcategories.includes(subcategory.toLowerCase())"
+                    @change="toggleSubcategory(subcategory)"
                     class="peer relative h-5 w-5 cursor-pointer appearance-none rounded border border-slate-300 shadow hover:shadow-md transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-slate-400 before:opacity-0 before:transition-opacity checked:border-slate-800 checked:bg-slate-800 checked:before:bg-slate-400 hover:before:opacity-10"
                     :disabled="loading"
                 />
                 <span
                     class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100"
                 >
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-3.5 w-3.5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-width="1"
-              >
-                <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                ></path>
-              </svg>
-            </span>
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-3.5 w-3.5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      stroke="currentColor"
+                      stroke-width="1"
+                  >
+                    <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </span>
               </label>
               <label
                   class="cursor-pointer text-slate-600 text-sm"
@@ -211,8 +155,9 @@
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import NavbarView from "@/views/NavbarView.vue";
 
 // State
 const openCategory = ref(null);
@@ -220,16 +165,8 @@ const isSidebarOpen = ref(false);
 const loading = ref(false);
 const results = ref({});
 const totalDocuments = ref(0);
-const selectedCategories = reactive({});
-
-// Categories definition
-const categories = {
-  'DevOps': ['Jenkins', 'Docker', 'Kubernetes', 'GitLab'],
-  'Database': ['MySQL', 'PostgreSQL', 'MongoDB', 'Redis'],
-  'Application': ['NodJS', 'Python', 'Java', 'PHP'],
-  'Framework': ['Symfony', "VueJS"],
-  'Web-server': ['Nginx', 'Apache', 'Tomcat']
-};
+const categories = ref([]);
+const selectedSubcategories = ref([]);
 
 // Methods
 const toggleCategory = (category) => {
@@ -244,37 +181,36 @@ const closeSidebar = () => {
   isSidebarOpen.value = false;
 };
 
-const isSubcategorySelected = (category, subcategory) => {
-  return selectedCategories[category]?.[subcategory] || false;
-};
+const toggleSubcategory = async (subcategory) => {
+  const lowercaseSubcat = subcategory.toLowerCase();
+  const index = selectedSubcategories.value.indexOf(lowercaseSubcat);
 
-const toggleSubcategory = async (category, subcategory) => {
-  if (!selectedCategories[category]) {
-    selectedCategories[category] = {};
+  if (index === -1) {
+    selectedSubcategories.value.push(lowercaseSubcat);
+  } else {
+    selectedSubcategories.value.splice(index, 1);
   }
-  selectedCategories[category][subcategory] = !selectedCategories[category][subcategory];
+
   await fetchData();
 };
 
-const getSelectedSubcategories = () => {
-  const selected = [];
-  Object.entries(selectedCategories).forEach(([category, subcats]) => {
-    Object.entries(subcats).forEach(([subcat, isSelected]) => {
-      if (isSelected) {
-        selected.push(subcat.toLowerCase());
-      }
-    });
-  });
-  return selected;
+const fetchCategories = async () => {
+  try {
+    const response = await axios.get(import.meta.env.VITE_APIURL + '/categories');
+    categories.value = response.data;
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+  }
 };
 
 const fetchData = async () => {
   loading.value = true;
   try {
-    const selectedSubcats = getSelectedSubcategories();
     const response = await axios.get(
         import.meta.env.VITE_APIURL + `/category${
-            selectedSubcats.length ? `?categories=${selectedSubcats.join(',')}` : ''
+            selectedSubcategories.value.length
+                ? `?categories=${selectedSubcategories.value.join(',')}`
+                : ''
         }`
     );
     results.value = response.data.results;
@@ -288,6 +224,7 @@ const fetchData = async () => {
 
 // Lifecycle hooks
 onMounted(async () => {
+  await fetchCategories();
   await fetchData();
 });
 </script>
--- src/views/NavbarView.vue
+++ src/views/NavbarView.vue
@@ -1,97 +1,16 @@
 <script setup>
-import { ref } from 'vue'
-
-const isMenuOpen = ref(false)
-
-const toggleMenu = () => {
-  isMenuOpen.value = !isMenuOpen.value
-}
 </script>
 
 <template>
-  <nav class="bg-white shadow dark:bg-gray-800 relative z-50">
-    <!-- Mobile Menu Button -->
-    <div class="lg:hidden flex justify-end p-4">
-      <button
-        @click="toggleMenu"
-        class="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 focus:outline-none"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            v-if="!isMenuOpen"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-          <path
-            v-else
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+  <nav class="block w-full  px-4 py-2 mx-auto bg-white shadow-md lg:px-8 lg:py-3 ">
+    <div class="justify-items-center">
+      <a href="#" class="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
+        <img class="w-28 rounded-md" :src="`../logo/LogoEasySetup.png`" />
+      </a>
     </div>
-
-    <!-- Desktop Menu -->
-    <div class="hidden lg:flex container items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-      <RouterLink
-        v-for="(item, index) in ['home', 'apache', 'nodeJs', 'mongoDb', 'mysql']"
-        :key="index"
-        :to="`/${item}`"
-        class="mx-1.5 sm:mx-6"
-      >
-        <button
-          type="button"
-          class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-20 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-        >
-          {{ item === 'home' ? 'Home' : item }}
-        </button>
-      </RouterLink>
-    </div>
-
-    <!-- Mobile Menu (Sidebar) -->
-    <div
-      v-show="isMenuOpen"
-      class="lg:hidden fixed inset-y-0 left-0 w-64 bg-white dark:bg-gray-800 shadow-lg transform transition-transform duration-300 ease-in-out z-50"
-      :class="isMenuOpen ? 'translate-x-0' : '-translate-x-full'"
-    >
-      <div class="flex flex-col space-y-4 p-4">
-        <RouterLink
-          v-for="(item, index) in ['home', 'apache', 'nodeJs', 'mongoDb', 'mysql']"
-          :key="index"
-          :to="`/${item}`"
-          @click="toggleMenu"
-        >
-          <button
-            type="button"
-            class="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-6 py-2.5 mb-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:border-gray-700"
-          >
-            {{ item === 'home' ? 'Home' : item }}
-          </button>
-        </RouterLink>
-      </div>
-    </div>
-
-    <!-- Overlay for mobile menu -->
-    <div
-      v-if="isMenuOpen"
-      @click="toggleMenu"
-      class="lg:hidden fixed inset-0 bg-black bg-opacity-50 z-40"
-    ></div>
   </nav>
 </template>
 
 <style scoped>
-.router-link-active button {
-  @apply bg-blue-600 dark:bg-blue-700;
-}
+
 </style>
